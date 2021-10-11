5a8f75fd31fe11fc2315cda2aa536f83ec5141d8
Makar Solomatin <makar.solomatin.5@yandex.ru>
2021-10-11 22:03:47 +0300

    intermediate

--- collect/utils.py
+++ collect/utils.py
@@ -62,9 +62,15 @@ def train_routes(*,
             'layer_id': Layer.ROUTE_SELECTION.value,
             'rid': str(rid),
         }
-        client.headers.update({
-            'User-Agent': UserAgent().firefox
-        })
+        u = UserAgent(cache=False).chrome
         return client.get(TIMETABLE_ADDRESS, params=params, headers={
-            'User-Agent': 'Mozilla/5.0',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'en-US,en;q=0.5',
+            'User-Agent': u,
+            'Host': 'pass.rzd.ru',
+            'Sec-Fetch-Dest': 'document',
+            'Sec-Fetch-Mode': 'navigate',
+            'Sec-Fetch-Site': 'none',
+            'Sec-Fetch-User': '?1',
         }).json()

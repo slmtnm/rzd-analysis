fe5477137d3ee19f3c5078fcfe87777cd6045b20
Makar Solomatin <makar.solomatin.5@yandex.ru>
2021-12-02 00:21:26 +0300

    Update collection script

--- tests/test_collect.py
+++ tests/test_collect.py
@@ -1,18 +1,10 @@
-import pytest
-from collect.utils import station_code, train_routes, Direction
-
-
-def test_station_code():
-    assert station_code('Санкт-Петербург') == 2004000
-    with pytest.raises(ValueError):
-        station_code('Неверное название')
+from collect import collect
 
 
 def test_train_routes():
-    res = train_routes(
+    collect(
         from_code='2004000',
         where_code='2000000',
         date='01.01.2022',
-        dir=Direction.ONEWAY,
-        with_transfers=False)
-    print(res)
+        proxy=None
+    )

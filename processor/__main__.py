8950f7c9c1ca7fd9da16046dd55ff32d2783b9f1
kystyn <konstantindamaskinskiy@gmail.com>
2021-12-09 03:29:20 +0300

    bugfix

--- processor/__main__.py
+++ processor/__main__.py
@@ -54,7 +54,7 @@ def main() -> None:
     city_from = [el['city'] for el in cities if el['code'] == charts[0].from_code][0]
     city_to = [el['city'] for el in cities if el['code'] == charts[0].where_code][0]
 
-    plot(charts[0], f'{charts[0].date.strftime("%d.%m.%Y")}. {city_from.title()} - {city_to.title()}',
+    plot(charts[0].points, f'{charts[0].date.strftime("%d.%m.%Y")}. {city_from.title()} - {city_to.title()}',
     'Количество дней до отправления' if sys.argv[3] == 'tariff' else 'Количество свободных мест'
     if sys.argv[3] == 'seats' else '')
 
